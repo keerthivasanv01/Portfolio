a31e078735c34f98f19bd653ea9d1c0e39ac020e
Keerthivasan V <149193330+keerthivasanv01@users.noreply.github.com>
2025-02-20 01:05:12 +0530

    Add files via upload

---
+++ assets/js/text.js
@@ -0,0 +1,7 @@
+var typed= new typed(".name" , {
+    Strings:["Front-end Developer" , "Back-end Developer" , "Web developer" , "Web Designer" , "Full-Stack Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+  });
